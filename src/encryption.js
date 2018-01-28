5b09008440fefbed40b4ff19c2ce0702429fe669
khapeterk <khapeterk@gmail.com>
2018-01-28 17:04:28 -0500

    put each character of index on its own line, to get close to a for loop structure

--- src/encryption.js
+++ src/encryption.js
@@ -1,12 +1,20 @@
 function encrypt(input) {
   let result = ""
   if (input === "feedthedog") return "fto ehg ee dd"
-  result += input[0] + input[0 + 1 * 4] + input[0 + 2 * 4]
+  result += input[0]
+  result += input[0 + 1 * 4]
+  result += input[0 + 2 * 4]
   result += " "
-  result += input[1] + input[1 + 1 * 4] + input[1 + 2 * 4]
+  result += input[1]
+  result += input[1 + 1 * 4]
+  result += input[1 + 2 * 4]
   result += " "
-  result += input[2] + input[2 + 1 * 4] + input[2 + 2 * 4]
+  result += input[2]
+  result += input[2 + 1 * 4]
+  result += input[2 + 2 * 4]
   result += " "
-  result += input[3] + input[3 + 1 * 4] + input[3 + 2 * 4]
+  result += input[3]
+  result += input[3 + 1 * 4]
+  result += input[3 + 2 * 4]
   return result
 }

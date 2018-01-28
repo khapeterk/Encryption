8d3cd391622453c2bb98bcc1044bd29944f61038
khapeterk <khapeterk@gmail.com>
2018-01-28 16:57:29 -0500

    test a change in code that is a step towards general implementation

--- src/encryption.js
+++ src/encryption.js
@@ -1,7 +1,7 @@
 function encrypt(input) {
   let result = ""
   if (input === "feedthedog") return "fto ehg ee dd"
-  result += "hae"
+  result += input[0] + input[4] + input[8]
   result += " "
   result += "and"
   result += " "

773318505e475c5e23f95d2f598328de4de9457f
khapeterk <khapeterk@gmail.com>
2018-01-28 17:05:29 -0500

    make all the characters look the same, to clearly see the pattern

--- src/encryption.js
+++ src/encryption.js
@@ -1,19 +1,19 @@
 function encrypt(input) {
   let result = ""
   if (input === "feedthedog") return "fto ehg ee dd"
-  result += input[0]
+  result += input[0 + 0 * 4]
   result += input[0 + 1 * 4]
   result += input[0 + 2 * 4]
   result += " "
-  result += input[1]
+  result += input[1 + 0 * 4]
   result += input[1 + 1 * 4]
   result += input[1 + 2 * 4]
   result += " "
-  result += input[2]
+  result += input[2 + 0 * 4]
   result += input[2 + 1 * 4]
   result += input[2 + 2 * 4]
   result += " "
-  result += input[3]
+  result += input[3 + 0 * 4]
   result += input[3 + 1 * 4]
   result += input[3 + 2 * 4]
   return result

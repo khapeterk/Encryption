b9706c4ce2884b891f394dbaaabd2f97e7a4710e
khapeterk <khapeterk@gmail.com>
2018-01-28 17:02:44 -0500

    generalized implementation of increments of indexes in a block

--- src/encryption.js
+++ src/encryption.js
@@ -1,12 +1,12 @@
 function encrypt(input) {
   let result = ""
   if (input === "feedthedog") return "fto ehg ee dd"
-  result += input[0] + input[0 + 4] + input[0 + 8]
+  result += input[0] + input[0 + 1 * 4] + input[0 + 2 * 4]
   result += " "
-  result += input[1] + input[1 + 4] + input[1 + 8]
+  result += input[1] + input[1 + 1 * 4] + input[1 + 2 * 4]
   result += " "
-  result += input[2] + input[2 + 4] + input[2 + 8]
+  result += input[2] + input[2 + 1 * 4] + input[2 + 2 * 4]
   result += " "
-  result += input[3] + input[3 + 4] + input[3 + 8]
+  result += input[3] + input[3 + 1 * 4] + input[3 + 2 * 4]
   return result
 }

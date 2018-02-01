b97b805fe26e4c60bfd33c917cfd442ff4c824d5
khapeterk <khapeterk@gmail.com>
2018-02-01 09:57:46 -0500

    convert result from string to array, and updated the blocks implementation accordingly

--- src/encryption.js
+++ src/encryption.js
@@ -2,22 +2,33 @@ function encrypt(input) {
   length = input.length
   rows = Math.floor(Math.sqrt(length))
   columns = Math.ceil(Math.sqrt(length))
-  let result = ""
+  let result = []
+  let block
   if (input === "feedthedog") return "fto ehg ee dd"
+
+  block = ""
   for(let rowIndex = 0; rowIndex < rows; rowIndex++) {
-    result += input[0 + rowIndex * columns]
+    block += input[0 + rowIndex * columns]
   }
-  result += " "
-  result += input[1 + 0 * 4]
-  result += input[1 + 1 * 4]
-  result += input[1 + 2 * 4]
-  result += " "
-  result += input[2 + 0 * 4]
-  result += input[2 + 1 * 4]
-  result += input[2 + 2 * 4]
-  result += " "
-  result += input[3 + 0 * 4]
-  result += input[3 + 1 * 4]
-  result += input[3 + 2 * 4]
-  return result
+  result.push(block)
+
+  block = ""
+  for(let rowIndex = 0; rowIndex < rows; rowIndex++) {
+    block += input[1 + rowIndex * columns]
+  }
+  result.push(block)
+
+  block = ""
+  for(let rowIndex = 0; rowIndex < rows; rowIndex++) {
+    block += input[2 + rowIndex * columns]
+  }
+  result.push(block)
+
+  block = ""
+  for(let rowIndex = 0; rowIndex < rows; rowIndex++) {
+    block += input[3 + rowIndex * columns]
+  }
+  result.push(block)
+
+  return result.join(" ")
 }
